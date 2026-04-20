a40ea8d036f74cd772969c1e06936c23b6ccf728
thesanjaysargam <sanjay@rapydlaunch.com>
2026-04-20 15:53:50 +0530

    feat: resume

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "play-lh.googleusercontent.com",
+      "media.licdn.com",
+    ],
+  },
 };
 
 export default nextConfig;
