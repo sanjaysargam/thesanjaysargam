be3884f6201e4bb539348f2357a40b7f26360dfc
sanjaysargam <sargamsanjaykumar@gmail.com>
2026-04-27 03:15:49 +0530

    done

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -235,7 +235,7 @@ I focus on building scalable apps, improving user experience, and writing clean,
           icon: <Icons.playstore className="size-3" />,
         },
       ],
-      image: "",
+      image: "https://chatgpt.com/backend-api/estuary/content?id=file_00000000e6207206bb72f011dff2f728&ts=493670&p=fs&cid=1&sig=b1f4ccaee3b1eaecd711a807e4934336e4c71d8ebc044c9797d14342b8db2e40&v=0",
       video: "",
     },
     {
